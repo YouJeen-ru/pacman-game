7af8e0b8d8deabfa482b50668e002b5b3619fb2e
YouJeen-ru <1.rouch@mail.ru>
2021-07-02 14:36:22 +0300

    Class GameBoard ui realized

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+import {LEVEL, OBJECT_TYPE} from './setup'
+
+// Classes
+import GameBoard from './GameBoard'
+
+
+// Dom Elements
+const gameGrid = document.querySelector('#game')
+const scoreTable = document.querySelector('#score')
+
+// Game Constants
+
+const POWER_PILL_TIME = 10000 //ms
+const GLOBAL_SPEED = 80 // ms
+const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL)
+
+// Initial setup
+let score = 0
+let timer = null
+let gameWin = false
+let powerPillActive = false
+let powerPillTimer = null
+
 

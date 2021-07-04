9b5a74aff5ac0fe87bc729433560a32023960601
YouJeen-ru <1.rouch@mail.ru>
2021-07-04 18:38:27 +0300

    Class Pacman ui realized

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import {LEVEL, OBJECT_TYPE} from './setup'
 
 // Classes
 import GameBoard from './GameBoard'
+import Pacman from './Pacman'
 
 
 // Dom Elements
 const gameGrid = document.querySelector('#game')
 const scoreTable = document.querySelector('#score')
+const startButton = document.querySelector('#start-button')
 
 // Game Constants
 
@@ -21,4 +23,32 @@ let gameWin = false
 let powerPillActive = false
 let powerPillTimer = null
 
+function gameOver(pacman, grid) {
 
+}
+
+function checkCollision(pacman, ghosts) {
+
+}
+
+function gameLoop(pacman, ghosts) {
+
+}
+
+function startGame() {
+    gameWin = false
+    powerPillActive = false
+    score = 0
+
+    startButton.classList.add('hide')
+
+    gameBoard.createGrid(LEVEL)
+    const pacman = new Pacman(2, 287)
+    gameBoard.addObject(287, [OBJECT_TYPE.PACMAN])
+    document.addEventListener('keydown', (e) => {
+        pacman.handleKeyInput(e, gameBoard.objectExist.bind(gameBoard))
+    })
+}
+
+//Initialize game
+startButton.addEventListener('click', startGame)
